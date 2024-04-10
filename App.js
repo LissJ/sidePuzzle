f8b366419953f624291de051c268245184262ef4
Liss <lissj014@gmail.com>
2024-04-10 10:32:07 -0300

    design arrumado
    eba :)

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Dimensions, Alert, Button } from 'react-native';
 import { Ionicons } from '@expo/vector-icons/';
+import { useNavigation } from '@react-navigation/native';
 
 const { width } = Dimensions.get('window');
-const TILE_SIZE = width / 3;
+const TILE_SIZE = width / 4;
 
 const initialTiles = [
   [1, 2, 3],
@@ -93,7 +94,7 @@ const App = () => {
     return (
       <TouchableOpacity
         key={value}
-        style={[styles.tile, { backgroundColor: value === null ? 'white' : 'lightblue' }]}
+        style={[styles.tile, { backgroundColor: value === null ? 'white' : '#EDCF72' }]}
         onPress={() => moveTileToEmptySpace(rowIndex, colIndex)}
       >
         <Text style={styles.tileText}>{value}</Text>
@@ -125,37 +126,97 @@ const App = () => {
     }
   };
 
+  const navigation = useNavigation();
+
   return (
-    <View style={styles.container}>
-      <View style={styles.board}>
-        {tiles.map((row, rowIndex) => (
-          <View key={rowIndex} style={styles.row}>
-            {row.map((tile, colIndex) => renderTile(rowIndex, colIndex, tile))}
+    <View style={styles.header}>
+      <View style={styles.container}>
+        <Text style={styles.jogos}>
+          Side Puzzle
+          <View style={styles.iconeHome}>
+            <Ionicons name="home-outline" 
+            style={styles.iconeInicio}
+            // onPress={toHome} 
+            />
           </View>
-        ))}
+        </Text>
+        <Text style={styles.resumoJogos}>
+          Uma atividade que envolve ordernar os números em ordem crescente, testando a capacidade do jogador.
+        </Text>
       </View>
-      <View style={styles.controls}>
-        <TouchableOpacity onPress={() => moveTile(0, tiles)}>
-          <Ionicons name="arrow-up-outline" size={15} color="black" />
-        </TouchableOpacity>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-          <TouchableOpacity onPress={() => moveTile(2, tiles)}>
+      <View style={styles.container2}>
+        <View style={styles.board}>
+          {tiles.map((row, rowIndex) => (
+            <View key={rowIndex} style={styles.row}>
+              {row.map((tile, colIndex) => renderTile(rowIndex, colIndex, tile))}
+            </View>
+          ))}
+        </View>
+        <View style={styles.controls}>
+          <TouchableOpacity style={styles.controlButton} onPress={() => moveTile(0, tiles)}>
+            <Ionicons name="arrow-up-outline" size={15} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.controlButton} onPress={() => moveTile(1, tiles)}>
+            <Ionicons name="arrow-down-outline" size={15} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.controlButton} onPress={() => moveTile(2, tiles)}>
             <Ionicons name="arrow-back-outline" size={15} color="black" />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => moveTile(3, tiles)}>
+          <TouchableOpacity style={styles.controlButton} onPress={() => moveTile(3, tiles)}>
             <Ionicons name="arrow-forward-outline" size={15} color="black" />
           </TouchableOpacity>
+
         </View>
-        <TouchableOpacity onPress={() => moveTile(1, tiles)}>
-          <Ionicons name="arrow-down-outline" size={15} color="black" />
-        </TouchableOpacity>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  header: {
+    justifyContent: 'center',
+    display: 'flex',
+    flex: 1,
+    backgroundColor: '#3C4146'
+  },
   container: {
+    backgroundColor: '#fff',
+    height: 200,
+    // flex: 0.7,
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
+    paddingHorizontal: 20,
+    marginBottom: 10,
+  },
+  jogos: {
+    marginTop: 50,
+    marginLeft: 15,
+    fontSize: 23,
+    fontWeight: '800',
+    color: '#34393E'
+  },
+  iconeHome: {
+    fontSize: 27,
+    // marginLeft: 155,
+    marginLeft: 160,
+    color: "#34393E"
+  },
+  iconeInicio: {
+    fontSize: 24,
+    marginTop: 5,
+    // marginLeft: 155,
+    marginLeft: 160,
+    color: "#34393E"
+  },
+  resumoJogos: {
+    marginTop: 10,
+    marginLeft: 15,
+    marginRight: 15,
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#34393E'
+  },
+  container2: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
@@ -172,16 +233,23 @@ const styles = StyleSheet.create({
     height: TILE_SIZE,
     justifyContent: 'center',
     alignItems: 'center',
-    borderWidth: 2,
-    borderColor: 'black',
+    borderWidth: 1,
+    borderColor: '#252320',
   },
   tileText: {
     fontSize: 24,
   },
   controls: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '50%',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '80%',
+    marginTop: 20,
+  },
+  controlButton: {
+    backgroundColor: 'white', // Cor de fundo dos botões
+    padding: 10,
+    borderRadius: 5,
   },
 });
 
